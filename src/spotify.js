806bf494995c800e12d8e8c11119d6a9f5bb8997
Austin <akc257@nau.edu>
2021-01-13 17:34:15 -0800

    Fix API Connection
    Connection to API is still invalid, repair

--- src/spotify.js
+++ src/spotify.js
@@ -12,6 +12,4 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
-export const loginUrl = `${authEndpoint}?client_id={clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-    )}&response_type=token&show_dialog=true`;
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
